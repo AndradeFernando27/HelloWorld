cd3824ca0580d9d3fb15f00c300e525f3f69f4f3
Auto Logon <autologon@CSED.LAB>
2024-08-13 21:20:11 -0300

    Codigo da aula

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+/** 
+1. Tipos de variáveis var, int, short, long, decimal, double, bool e string;
+
+
+
+2. Operadores de atribuição, aritméticos, relacionais, lógicos e unários;
+
+3. Estruturas de decisão if/else, ternárias e switch;
+
+4. Estruturas de repetição while, do/while, for, foreach;
+
+5. Arrays e List.
+**/
+ 
+
+Console.WriteLine("Digite a nota 1:");
+var n1 = Console.ReadLine ();
+Console.WriteLine("Digite a nota 2:");
+var n2 = Console.ReadLine ();
+var media = (n1 + n2)\2;
+Console.WriteLine("nota 1 =" +n1  + "\n nota 2 =" + n2);
+Console.WriteLine("Media = "+ media);
